6f6dd9583d3266adbec83e5fc1ff0dcf1d9f0b09
zebbyG <zebbynjau@gmail.com>
2023-02-02 12:27:42 +0300

    add filtered_logger.py task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,10 +7,13 @@ from typing import List
 """
 required inputs
 """
+# task 0
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
@@ -23,6 +26,8 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
     """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+# task 1
 
 
 class RedactingFormatter(logging.Formatter):
@@ -47,3 +52,22 @@ class RedactingFormatter(logging.Formatter):
         message = super(RedactingFormatter, self).format(record)
         txt = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
         return txt
+
+# task 2
+
+
+def get_logger() -> logging.Logger:
+    """
+    Return a logging.Logger object
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+
+    formatter = RedactingFormatter(list(PII_FIELDS))
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
