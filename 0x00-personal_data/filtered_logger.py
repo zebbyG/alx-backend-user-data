9e9d3ef92f8afcb26b4f605925950f22a6c21d5e
zebbyG <zebbynjau@gmail.com>
2023-02-02 11:59:17 +0300

    try task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
-"""
-Regex-ing
+"""A module for filtering logs.
 """
 import re
-import typing
+import logging
+from typing import List
 """
-required module
+required inputs
 """
-# 0.Regex-ing task
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
 }
 
 
-def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     :param fields: a list of strings representing all fields to obfuscate
     :param redaction: a string representing by what the field will be obfuscated
@@ -24,3 +23,27 @@ def filter_datum(fields: typing.List[str], redaction: str, message: str, separat
     """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        initializing class
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        formats a LogRecord.
+        """
+        message = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
+        return txt
