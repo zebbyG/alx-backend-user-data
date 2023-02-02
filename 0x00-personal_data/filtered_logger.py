0534101e620c9ac097e17c6e8d0bc6059e911c65
zebbyG <zebbynjau@gmail.com>
2023-02-02 10:18:21 +0300

    test task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Regex-ing
+"""
+import re
+import typing
+"""
+required module
+"""
+            # 0.Regex-ing task done
+
+
+def filter_datum(fields: typing.List[str], redaction: str,
+                 message: str, separator: str):
+    """
+    :param fields: a list of strings representing all fields to obfuscate
+    :param redaction: a string representing by what the field will be obfuscated
+    :param message: a string representing the log line
+    :param separator: a string representing by which character is separating all fields in the log line (message)
+    :return: the log message obfuscated
+    """
+    for f in fields:
+        message = re.sub(f+'=.*?'+separator,
+                         f+'='+redaction+separator, message)
+        return message
