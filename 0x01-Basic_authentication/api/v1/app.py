820e39d0b60233fc0ecf1b77c89fcb0db1544f38
zebbyG <zebbynjau@gmail.com>
2023-02-07 14:23:03 +0300

    add 401 unauthorized handler

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,11 +21,11 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """Forbidden handler.
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Unauthorized handler.
     """
-    return jsonify({"error": "Forbidden"}), 403
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
